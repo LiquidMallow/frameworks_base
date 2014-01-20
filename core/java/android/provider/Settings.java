ccf9f704b8d9bce1a008d33a2ff170564043fe4e
XjCook <xjcook@gmail.com>
2014-01-20 16:57:25 +0100

    [2/2] Frameworks: Halo Color
    RR- Change the HoloBlue to the new gray that was already set
    
    Change-Id: I6b8a3e8b1057e466b947da63e5faf67560781837
    Signed-off-by: anerikg <anerikgames@gmail.com>
    Signed-off-by: scott <scott@ubuntu>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/HaloProperties.java
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7919,6 +7919,12 @@ public final class Settings {
         public static final String HALO_UNLOCK_PING = "halo_unlock_ping";
 
         /**
+         * HALO color, default is 0xffbbbbbb (normal)
+         * @hide
+         */
+        public static final String HALO_COLOR = "halo_color";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
