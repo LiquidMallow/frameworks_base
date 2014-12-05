73ebbc81f6688612efcc529991f1f4d5465683a2
Jon Haus <haus.xda@gmail.com>
2014-12-05 20:22:33 +0100

    Squashed: FWB: Network traffic meter (1/2)
    PS2: Make fonts smaller
    PS3: Remove useless colors.xml change
    
    Change-Id: I00f096784a238b3828c4e0a79ecfe410b69bc888
    
    FWB: Network traffic supercharged (1/2)
    
    ----
    
    Ported to MM by @iceandfire
    
    Change-Id: Ide3ac899da7632dd1fb3a0615ab9560a4ae6cfd0
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java
@@ -32,7 +32,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
     private final PhoneStatusBarView mView;
     private final float mIconAlphaWhenOpaque;
 
-    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mMinitBattery;
+    private View mLeftSide, mStatusIcons, mSignalCluster, mBattery, mClock, mNetworkTraffic, mMinitBattery;
     private Animator mCurrentAnimation;
 
     public PhoneStatusBarTransitions(PhoneStatusBarView view) {
@@ -51,7 +51,8 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
         mSignalCluster = mView.findViewById(R.id.signal_cluster);
         mBattery = mView.findViewById(R.id.battery);
         mClock = mView.findViewById(R.id.clock);
-	    mMinitBattery = mView.findViewById(R.id.minitBattery);
+	mNetworkTraffic = mView.findViewById(R.id.networkTraffic);
+	mMinitBattery = mView.findViewById(R.id.minitBattery);
         applyModeBackground(-1, getMode(), false /*animate*/);
         applyMode(getMode(), false /*animate*/);
     }
@@ -95,6 +96,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
                     animateTransitionTo(mLeftSide, newAlpha),
                     animateTransitionTo(mStatusIcons, newAlpha),
                     animateTransitionTo(mSignalCluster, newAlpha),
+					animateTransitionTo(mNetworkTraffic, newAlpha),
                     animateTransitionTo(mBattery, newAlphaBC),
                     animateTransitionTo(mClock, newAlphaBC),
                     animateTransitionTo(mMinitBattery, newAlphaBC)
@@ -108,6 +110,7 @@ public final class PhoneStatusBarTransitions extends BarTransitions {
             mLeftSide.setAlpha(newAlpha);
             mStatusIcons.setAlpha(newAlpha);
             mSignalCluster.setAlpha(newAlpha);
+	    mNetworkTraffic.setAlpha(newAlpha);
             mBattery.setAlpha(newAlphaBC);
             mClock.setAlpha(newAlphaBC);
             mMinitBattery.setAlpha(newAlphaBC);
