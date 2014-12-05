9f2e05ac1b88964e6ca0872b3d26bbf5a9daecd2
Danesh M <daneshm90@gmail.com>
2014-12-05 11:33:14 -0800

    Ability to hide alarm clock icon
    Change-Id: I452280ecb272df890ffb403301b398950210cd0a
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2355,6 +2355,11 @@ public final class Settings {
         };
 
         /**
+         * @hide
+         */
+        public static final String SHOW_ALARM_ICON = "show_alarm_icon";
+
+        /**
          * Scaling factor for fonts, float.
          */
         public static final String FONT_SCALE = "font_scale";
