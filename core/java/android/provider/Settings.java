b5e3b9de6d161ec06c04e607be1a3c7eb6044ce8
cristianomatos <cristianobmatos@gmail.com>
2014-12-29 16:04:53 -0200

    AOKP animations: Add an entry for TRANSIT_TASK_OPEN_BEHIND [1/2]
    Change-Id: I306c932c79d32e727f15fe1645ffa723d3169bec
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4448,24 +4448,6 @@ public final class Settings {
          */
         public static final String TOAST_ICON = "toast_icon";
 
-        /**  
-         * AOKP Custom System Animations
-         * @hide
-         */  
-        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {  
-                "activity_open",
-                "activity_close",
-                "task_open",  
-                "task_close",  
-                "task_to_front",
-                "task_to_back",  
-                "wallpaper_open",  
-                "wallpaper_close",  
-                "wallpaper_intra_open",  
-                "wallpaper_intra_close",  
-        };  
-        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
-
         /**
          * Change the screenshot delay
          * @hide
@@ -4907,6 +4889,25 @@ public final class Settings {
          * @hide
          */
         public static final String ALLOW_LIGHTS = "allow_lights";
+
+        /**  
+         * AOKP Custom System Animations
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+                "task_open_behind",
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
