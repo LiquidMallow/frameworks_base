0177d55140efb6fde074fe3c9f7917b042cdc882
Andrew Boren <andrew.boren@gmail.com>
2015-01-25 09:19:16 +0100

    Powermenu screenshot delay [1/2]
     * Only works from power menu
     * Defaults to 1 second (1000ms)
    
    Change-Id: Ic7477d4ed962b97d5bbfd980cfdb7e3516262463
    Signed-off-by: Varun <date.varun123@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4352,6 +4352,12 @@ public final class Settings {
         public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
 
         /**
+         * Change the screenshot delay
+         * @hide
+         */
+        public static final String SCREENSHOT_DELAY = "screenshot_delay";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
