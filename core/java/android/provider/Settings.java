fe96dae88edb0891ecdfe45f0cbfb9ab06644361
Adnan <adnan@cyngn.com>
2015-05-03 22:56:10 +0900

    SystemUI: HeadsUp blacklists (1/2)
    Screw'd:
    Modified for Screw'd AOSP. Without that CMSettings bullsh*t!
    
    Change-Id: I5bb9ead502d71182a689a3cb63f7ba9f8414d5b6
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4567,6 +4567,20 @@ public final class Settings {
          */
         public static final String DIM_NAV_BUTTONS_TOUCH_ANYWHERE = "dim_nav_buttons_touch_anywhere";
 
+		/**
+         * Which applications to disable heads up notifications in
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_CUSTOM_VALUES = "heads_up_custom_values";
+		
+		/**
+         * Which applications to disable heads up notifications for
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
