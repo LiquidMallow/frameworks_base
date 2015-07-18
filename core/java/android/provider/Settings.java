6dea2b114bb652cd882116f4123315aefd6add39
AdrianDC <radian.dc@gmail.com>
2015-07-18 12:20:51 +0200

    Lights with Screen On [1/2]
    Implement a setting allowing lights to be activated
    for new notifications even if the screen is on.
    
    Lights with screen on and Custom values are separated
    in an advanced section for a cleaner overview.
    
    This setting gives the user an oportunity to activate lights
    with the screen on and also during DayDream screensaver.
    The option is not activated by default.
    
    Change-Id: I2071147d8ddab80ba0e1e7310e785ac3e03b3c7c
    Signed-off-by: AdrianDC <radian.dc@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2979,6 +2979,14 @@ public final class Settings {
         public static final Validator SHOW_WEB_SUGGESTIONS_VALIDATOR = sBooleanValidator;
 
         /**
+         * Whether to allow notifications with the screen on or DayDreams.
+         * The value is boolean (1 or 0). Default will always be false.
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_SCREEN_ON =
+                "notification_light_screen_on_enable";
+
+        /**
          * Whether the notification LED should repeatedly flash when a notification is
          * pending. The value is boolean (1 or 0).
          * @hide
@@ -3464,7 +3472,8 @@ public final class Settings {
             VIBRATE_WHEN_RINGING,
             RINGTONE,
             LOCK_TO_APP_ENABLED,
-            NOTIFICATION_SOUND
+            NOTIFICATION_SOUND,
+            NOTIFICATION_LIGHT_SCREEN_ON
         };
 
         /**
