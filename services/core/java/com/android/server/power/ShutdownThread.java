1f51d3bcb1e41b90cb93a6c08d298c11c7a07dd0
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-08-05 01:51:33 +0100

    Themes: Theme power off and reboot dialog
    This patch allows theming of the dialog which
    comes after the power menu
    
    Change-Id: I81546543da13cf220d9247dbce833d6e883ed091

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -128,9 +128,10 @@ public final class ShutdownThread extends Thread {
      * @param confirm true if user confirmation is needed before shutting down.
      */
     public static void shutdown(final Context context, boolean confirm) {
+        final Context uiContext = getUiContext(context);
         mReboot = false;
         mRebootSafeMode = false;
-        shutdownInner(context, confirm);
+        shutdownInner(uiContext, confirm);
     }
 
     static void shutdownInner(final Context context, boolean confirm) {
@@ -210,11 +211,12 @@ public final class ShutdownThread extends Thread {
      * @param confirm true if user confirmation is needed before shutting down.
      */
     public static void reboot(final Context context, String reason, boolean confirm) {
+        final Context uiContext = getUiContext(context);
         mReboot = true;
         mRebootSafeMode = false;
         mRebootUpdate = false;
         mRebootReason = reason;
-        shutdownInner(context, confirm);
+        shutdownInner(uiContext, confirm);
     }
 
     /**
