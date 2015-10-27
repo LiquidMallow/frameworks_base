9b7d01933e0c1edd5d26e5d02261f9b264a070e0
Myself5 <myself5@carbonrom.org>
2015-10-27 21:03:42 +0100

    [1/2] fwb: Add ability to permanently hide apps from recents
    This is based off XposedAppSettings, but modified to fit our needs, and
    a little bit of: d9973ba0ef48d27688df1e6b33a9777b67b24142
    
    Change-Id: Id1a599df90f3c12ccd32d69eb2e262a5d8f464ab
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4929,6 +4929,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
 
         /**
+         *  List of apps hidden from recents
+         * @hide
+         */
+        public static final String HIDE_FROM_RECENTS_LIST = "hide_from_recents_list";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
