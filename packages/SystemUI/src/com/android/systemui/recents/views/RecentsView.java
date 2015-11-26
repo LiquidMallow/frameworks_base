9c799099679c13b6d4f348681722341ec324fe3d
cristianomatos <cristianobmatos@gmail.com>
2015-11-26 08:02:58 -0200

    Adjustments in RAM Bar
    Change-Id: Ib231645fb90f99a9b58c2da240a1eeb59b247a65
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -463,7 +463,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         return true;
     }
 
-    private void updateMemoryStatus() {
+    public void updateMemoryStatus() {
         if (mMemText.getVisibility() == View.GONE
                 || mMemBar.getVisibility() == View.GONE) return;
 
@@ -502,6 +502,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         });
         mMemText = (TextView) ((View)getParent()).findViewById(R.id.recents_memory_text);
         mMemBar = (ProgressBar) ((View)getParent()).findViewById(R.id.recents_memory_bar);
+
+        updateMemoryStatus();
     }
 
     /**
@@ -808,6 +810,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         }
 
         mCb.onAllTaskViewsDismissed();
+
+        updateMemoryStatus();
 
         // Keep track of all-deletions
         MetricsLogger.count(getContext(), "overview_task_all_dismissed", 1);
