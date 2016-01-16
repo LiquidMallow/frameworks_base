dedb63c40fd2b828b17979fc298243089523579e
Lars Greiss <kufikugel@googlemail.com>
2016-01-16 09:44:42 +0000

    Less notifications sound [1/2]
    Change-Id: Idfb0f33aef12e72af9d7be0fc74708f747654eed
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3421,6 +3421,12 @@ public final class Settings {
          */
         public static final String DOZE_BRIGHTNESS = "doze_brightness";
 
+		/**
+         * Whether to mute annoying notifications
+         * @hide
+         */
+        public static final String MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD = "mute_annoying_notifications_threshold";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
