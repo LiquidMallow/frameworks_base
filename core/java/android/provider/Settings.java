1c6fe8407bcc30c93d4dd2816fe15166ecb46da9
xyyx <xyyx@mail.ru>
2016-01-29 11:22:41 +0900

    SystemUI: HeadsUp whitelists (1/2)
    All notifications from app in whitelists will be posted to headsup.
    
    Screw'd:
    Modified for Screw'd AOSP - Without that CMSettings bullsh*t!
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4580,6 +4580,13 @@ public final class Settings {
          * @hide
          */
         public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
+		
+		/**
+         * Which applications to post all notification to heads up
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_WHITELIST_VALUES = "heads_up_whitelist_values";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
