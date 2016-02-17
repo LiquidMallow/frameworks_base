f1a39bee52a417ef3b7de56a63afc199651be619
Ankit Gupta <agupta@cyngn.com>
2016-02-17 15:48:17 -0800

    SettingsProvider: Allow default volume adjust sound to be overlayed.
    Issue-id: YAM-145
    Change-Id: I06394c4fcfca5978b50a4186935ac6bda7b94b16
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5043,6 +5043,12 @@ public final class Settings {
          */
         public static final String LOCK_SCREEN_VISUALIZER_CUSTOM_COLOR = "lock_screen_visualizer_custom_color";
 
+	/**
+         * Whether the phone volume up / down effect should be played
+         * @hide
+         */
+        public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
+
         /**
          * Whether to use the proximity sensor to turn the screen on/off during a call
          * @hide
