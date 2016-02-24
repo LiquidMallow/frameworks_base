52f4fe2ffaeb5c9194152703e0b177753ea5f843
tiger_huang <tiger_huang@htc.com>
2016-02-24 17:28:53 +0800

    Make the config of WindowState synced with the client side
    The original logic may cause WMS to skip sending the configuration to
    the client side unexpectedly. For example, a portrait app is resuming
    from a landscape app, this portrait app may get the landscape config
    from relayoutWindow() first. However, WMS would not change the config
    of WindowState to the one returned to the client side. When AMS sets
    the new config (the portrait one) to WMS, WMS won't report the new
    config to the portrait app, because the config of the WindowState and
    the config of WMS may be the same (portrait). The latest config from
    onConfigurationChanged() of the portrait app would be landscape... :(
    
    Note: performLayoutAndPlaceSurfacesLockedInner() would be skipped if
    mWaitingForConfig is true, that makes WMS skip setting the server
    config to the WindowState in relayoutWindow().
    
    Change-Id: Ifb08b1e7b0ed5fda8fba5c7253f496c55665580e

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -3298,6 +3298,10 @@ public class WindowManagerService extends IWindowManager.Stub
                         if (DEBUG_CONFIGURATION) Slog.i(TAG, "Window " + win
                                 + " visible with new config: " + mCurConfiguration);
                         outConfig.setTo(mCurConfiguration);
+                        final TaskStack stack = win.getStack();
+                        final Configuration overrideConfig =
+                                (stack != null) ? stack.mOverrideConfig : Configuration.EMPTY;
+                        win.setConfiguration(mCurConfiguration, overrideConfig);
                     }
                 }
                 if ((attrChanges&WindowManager.LayoutParams.FORMAT_CHANGED) != 0) {
