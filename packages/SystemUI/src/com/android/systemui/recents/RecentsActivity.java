DirtyUnicorns/android_frameworks_base

Immersive Recents [1/2]
This adds on to previous mod called FULL SCREEN RECENTS but now adds options
to show recents with statusbar only or navbar only as well as fullscreen.

- Renamed the mod to immersive recents to avoid confusion moving forward

0 - Default AOSP
1 - Fullscreen
2 - Statusbar only
3 - NavBar only

Change-Id: Ifff1882ca9045773d257f009674a88f125b22763

packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -30,6 +30,7 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewStub;
@@ -225,7 +226,7 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
                         R.anim.recents_to_launcher_enter,
                     mConfig.launchedFromSearchHome ? R.anim.recents_to_search_launcher_exit :
                         R.anim.recents_to_launcher_exit));
-        setFullScreen();
+        setImmersiveRecents();
 
         // Mark the task that is the launch target
         int taskStackCount = stacks.size();
@@ -617,17 +618,37 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
         }
     }
 
-    private void setFullScreen() {
-       if (Settings.System.getInt(getContentResolver(),
-           Settings.System.RECENTS_FULL_SCREEN, 0) == 1) {
+    private void setImmersiveRecents() {
+        boolean isPrimary = UserHandle.getCallingUserId() == UserHandle.USER_OWNER;
+        int immersiveRecents = isPrimary ? getImmersiveRecents() : 0;
+
+        if (immersiveRecents == 0) {
+         // default AOSP action
+        }
+        if (immersiveRecents == 1) {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        } else {
-        // do nothing at all for now
         }
+        if (immersiveRecents == 2) {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+        if (immersiveRecents == 3) {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    private int getImmersiveRecents() {
+        return Settings.System.getInt(getContentResolver(),
+                Settings.System.IMMERSIVE_RECENTS, 0);
     }
 
     /**** RecentsResizeTaskDialog ****/

LiquidMallow/frameworks_base

Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
Conflicts:
	core/java/android/provider/Settings.java
	packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java

packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -29,6 +29,7 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewStub;
@@ -226,7 +227,7 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
                         R.anim.recents_to_launcher_enter,
                     mConfig.launchedFromSearchHome ? R.anim.recents_to_search_launcher_exit :
                         R.anim.recents_to_launcher_exit));
-        setFullScreen();
+        setImmersiveRecents();
 
         // Mark the task that is the launch target
         int taskStackCount = stacks.size();
@@ -596,18 +597,37 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
                 Toast.LENGTH_SHORT).show();
         }
     }
+    private void setImmersiveRecents() {
+        boolean isPrimary = UserHandle.getCallingUserId() == UserHandle.USER_OWNER;
+        int immersiveRecents = isPrimary ? getImmersiveRecents() : 0;
 
-    private void setFullScreen() {
-       if (Settings.System.getInt(getContentResolver(),
-           Settings.System.RECENTS_FULL_SCREEN, 0) == 1) {
+        if (immersiveRecents == 0) {
+         // default AOSP action
+        }
+        if (immersiveRecents == 1) {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        } else {
-        // do nothing at all for now
         }
+        if (immersiveRecents == 2) {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+        if (immersiveRecents == 3) {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    private int getImmersiveRecents() {
+        return Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.IMMERSIVE_RECENTS, 0, UserHandle.USER_CURRENT);
     }
 
     /**** RecentsResizeTaskDialog ****/