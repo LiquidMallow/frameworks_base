cf83fccda14455720ea208412ba943dcaf67b3a5
liquid0624 <rjbirnesser@gmail.com>
2016-02-28 20:10:40 -0500

    systemui: remove duplicate keyguard rotation bool

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarWindowManager.java
@@ -50,7 +50,6 @@ public class StatusBarWindowManager {
     private WindowManager.LayoutParams mLpChanged;
     private int mBarHeight;
     private boolean mKeyguardScreenRotation;
-    private final boolean mKeyguardScreenRotation;
     private final float mScreenBrightnessDoze;
 	
     private final State mCurrentState = new State();
