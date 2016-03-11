e9d8805fc409ca97fe39c7320232575f963ef4fc
Yang Ni <yangni@google.com>
2016-03-10 16:12:31 -0800

    Correctly init code cache path for RS
    Bug: 27439261
    
    Script Group needs to know the code cache path before it can call bcc to merge
    kernels. However, the code cache path is now initialized by ScriptC class.
    
    In the case where a script group (or even the entire app) does not contain any
    regular script but only intrinsics, the code cache would remain uninitialized.
    
    Fixed this by initializing code cache path in RenderScript class when the
    context is created.
    
    Change-Id: I87f9e62e0f3b479f94e43daa3e9695a5b38710db

--- rs/java/android/renderscript/RenderScript.java
+++ rs/java/android/renderscript/RenderScript.java
@@ -1345,12 +1345,19 @@ public class RenderScript {
     }
 
     /**
+     * Name of the file that holds the object cache.
+     */
+    private static String mCachePath;
+
+    String getCachePath() { return mCachePath; }
+
+    /**
      * Create a RenderScript context.
      *
      * @param ctx The context.
      * @return RenderScript
      */
-    private static RenderScript internalCreate(Context ctx, int sdkVersion, ContextType ct, int flags) {
+    private static synchronized RenderScript internalCreate(Context ctx, int sdkVersion, ContextType ct, int flags) {
         if (!sInitialized) {
             Log.e(LOG_TAG, "RenderScript.create() called when disabled; someone is likely to crash");
             return null;
@@ -1374,7 +1381,7 @@ public class RenderScript {
         // set up cache directory for entire context
         final String CACHE_PATH = "com.android.renderscript.cache";
         File f = new File(RenderScriptCacheDir.mCacheDir, CACHE_PATH);
-        String mCachePath = f.getAbsolutePath();
+        mCachePath = f.getAbsolutePath();
         f.mkdirs();
         rs.nContextSetCacheDir(mCachePath);
 
