acde26c0cc2f5e5dcf2eb7aea413d4cbba539525
rogersb11 <brettrogers11@gmail.com>
2016-03-15 00:43:15 -0400

    Dashboard Text Size 2/2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5017,6 +5017,18 @@ public final class Settings {
         public static final String IN_CALL_PROXIMITY_SENSOR = "in_call_proximity_sensor";
 
         /**
+         * Dashboard title text size
+         * @hide
+         */
+        public static final String SETTINGS_TITLE_TEXT_SIZE = "settings_title_text_size";
+
+        /**
+         * Dashboard category text size
+         * @hide
+         */
+        public static final String SETTINGS_CATEGORY_TEXT_SIZE = "settings_category_text_size";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
