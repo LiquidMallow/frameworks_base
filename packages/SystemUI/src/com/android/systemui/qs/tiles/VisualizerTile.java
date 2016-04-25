15a18fb53ceeee81bda53be744d767d241179052
d34d <clark@cyngn.com>
2016-04-25 08:16:03 -0700

    SysUI: Use VectorDrawable's for rendering battery
    The current implementation of BatteryMeterView uses arrays of points
    that are drawn programmatically.  This limits the ability to overlay
    or theme the battery.  By utilizing VectorDrawables and
    AnimatedVectorDrawables we can achieve the same look and feel of
    stock while allowing for more unique battery meters.
    
    Change-Id: I16a8a949a92827a9e8c1b528981031b6830e97cd
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    
    Conflicts:
    	packages/SystemUI/Android.mk
    	packages/SystemUI/res/layout/system_icons.xml
    	packages/SystemUI/res/values/cm_strings.xml
    	packages/SystemUI/res/values/cm_styles.xml
    	packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
    	packages/SystemUI/src/com/android/systemui/DockBatteryMeterView.java

--- packages/SystemUI/src/com/android/systemui/qs/tiles/VisualizerTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/VisualizerTile.java
@@ -41,6 +41,7 @@ import com.pheelicks.visualizer.AudioData;
 import com.pheelicks.visualizer.FFTData;
 import com.pheelicks.visualizer.VisualizerView;
 import com.pheelicks.visualizer.renderer.Renderer;
+import com.android.internal.logging.MetricsLogger;
 
 public class VisualizerTile extends QSTile<QSTile.BooleanState>  implements KeyguardMonitor.Callback {
 
