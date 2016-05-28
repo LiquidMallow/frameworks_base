c4d3f79cc4ca3826103ee790de71ee3750f143c2
Randall Rushing <randall.rushing@gmail.com>
2016-05-27 20:10:58 -0700

    Headers: Separate default header handling from contextual headers
    Initially, the headers overlay was designed to allow header packs
    to overlay the default header background as well as the contextual
    header series of drawables. Upon reflection, this design concept
    if found lacking. The headers overlay will now only affect
    contextual headers and NOT the default header drawable. The default
    header will get resources from the statusbar overlay just as before.
    
    Change-Id: I0448add8f986d4f33033b9f914861bfc7919bcd1
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1331,6 +1331,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         ThreadedRenderer.overrideProperty("ambientRatio", String.valueOf(1.5f));
 
         mStatusBarHeaderMachine.addObserver(mHeader);
+        mStatusBarHeaderMachine.updateResources(mContext.getResources(), StatusBarHeaderMachine.STATUSBAR_RESOURCES);
         mStatusBarHeaderMachine.forceUpdate();
         return mStatusBarView;
     }
@@ -3814,7 +3815,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         SettingsObserver observer = new SettingsObserver(mHandler);
         if (newTheme != null) mCurrentTheme = (ThemeConfig) newTheme.clone();
         if (updateHeaders) {
-            mStatusBarHeaderMachine.updateResources(getHeadersThemedResources());
+            mStatusBarHeaderMachine.updateResources(getHeadersThemedResources(), StatusBarHeaderMachine.HEADER_RESOURCES);
         }
         if (updateStatusBar) {
             recreateStatusBar();
