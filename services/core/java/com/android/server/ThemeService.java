4acf770247dc743d3bc1eda3af43b10911a8c9f2
d34d <clark@cyngn.com>
2016-06-17 16:23:38 -0700

    Themes: Clear out bootanimation if pkgName empty An empty string for the package name implies the given component should be cleared out, so make sure updateBootAnim respects that.
    Change-Id: I07c7fc64970546c2d73a610d9c0b71f1e757657d
    TICKET: CRACKLING-1043
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- services/core/java/com/android/server/ThemeService.java
+++ services/core/java/com/android/server/ThemeService.java
@@ -571,7 +571,7 @@ public class ThemeService extends IThemeService.Stub {
     }
 
     private boolean updateBootAnim(String pkgName) {
-        if (SYSTEM_DEFAULT.equals(pkgName)) {
+        if (TextUtils.isEmpty(pkgName) || SYSTEM_DEFAULT.equals(pkgName)) {
             clearBootAnimation();
             return true;
         }
